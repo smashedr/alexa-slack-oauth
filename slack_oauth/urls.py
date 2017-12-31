44f9ae6dc617ca7893c699753b343596196bc966
Shane <shane@shanerice.me>
2017-12-31 01:23:30 -0800

    Updated urls

--- slack_oauth/urls.py
+++ slack_oauth/urls.py
@@ -1,10 +1,17 @@
-from django.contrib import admin
+from django.conf import settings
 from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from home import views as home
 
 urlpatterns = [
-    url(r'^$', home.do_connect, name='home'),
+    url(r'^$', RedirectView.as_view(
+        url=settings.CONFIG.get('App', 'home_site', raw=True)
+    ), name='home'),
+    url(r'^favicon\.ico$', RedirectView.as_view(
+        url=settings.STATIC_URL + 'images/favicon.ico'
+    )),
     url(r'connect/', home.do_connect, name='connect'),
     url(r'authenticate/', home.do_login, name='authenticate'),
     url(r'token/', home.get_token, name='token'),
